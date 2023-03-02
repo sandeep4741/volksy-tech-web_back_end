a6665218b33b82d7b17031a3a57fd914c4594dd2
sandeep4741 <sandeepg@brightcomgroup.com>
2023-03-01 22:06:53 -0800

    100

--- ES6_basic/100-createIteratorObject.js
+++ ES6_basic/100-createIteratorObject.js
@@ -1,11 +1,5 @@
 export default function createIteratorObject(report) {
-    const emps = [];
-    // eslint-disable-next-line
-      for (const dep of Object.keys(report.allEmployees)) {
-      // eslint-disable-next-line
-          for (const emp of report.allEmployees[dep]) {
-        emps.push(emp);
-      }
-    }
-    return emps;
+  while (report) {
+    console.log('Placeholder');
   }
+}

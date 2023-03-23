c9083709ad462e87da30970cba42970e48b766c6
sandeep4741 <sandeepg@brightcomgroup.com>
2023-03-23 03:44:05 -0700

    9

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,14 +1,47 @@
+'use strict';
 const request = require('request');
-const chai = require('chai')
-const { expect } = require('chai');
+const chai = require('chai');
 
-describe('API test', () => {
-      it('respond with 200 code and body', (done) => {
-        request('http://localhost:7865', 'GET', (err, res, body) => {
-          if (err) throw err;
-          expect(res.statusCode).to.equal(200);
-          expect(body).to.equal('Welcome to the payment system');
-        });
+describe('basic integration testing', () => {
+  describe('GET /', () => {
+    it('endpoint: GET /', (done) => {
+      const call = {
+        url: 'http://localhost:7865',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Welcome to the payment system');
         done();
+      });
     });
+  });
+});
+
+describe('regex integration testing', () => {
+  describe('GET /cart/:id', () => {
+    it('endpoint: GET /cart/:id', (done) => {
+      const call = {
+        url: 'http://localhost:7865/cart/12',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        chai.expect(response.statusCode).to.equal(200);
+        chai.expect(body).to.equal('Payment methods for cart 12');
+        done();
+      });
+    });
+  });
+  describe('GET /cart/:isNaN', () => {
+    it('endpoint: GET /cart/:isNaN', (done) => {
+      const call = {
+        url: 'http://localhost:7865/cart/anything',
+        method: 'GET',
+      };
+      request(call, (error, response, body) => {
+        chai.expect(response.statusCode).to.equal(404);
+        done();
+      });
+    });
+  });
 });
